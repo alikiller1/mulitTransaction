9967b201f647f5b09c94839256c4299c775a2ad1
alikiller1 <liu2011321@sina.com>
2020-10-28 18:07:12 +0800

    aaa

--- src/test/java/com/liuqh/mulittransaction/service/ServiceTest.java
+++ src/test/java/com/liuqh/mulittransaction/service/ServiceTest.java
@@ -16,7 +16,7 @@ import com.liuqh.mulittransaction.service.ds2.OrderService;
  * 2020年10月23日 下午6:13:31
  */
 
-public class ServieTest extends BaseTest {
+public class ServiceTest extends BaseTest {
 	@Autowired
 	private ProductService productService;
 	
@@ -48,7 +48,7 @@ public class ServieTest extends BaseTest {
 	@Test
 	public void testInsertProductAndOrder() {
 		Product p=new Product();
-		p.setPName("iphon12");
+		p.setPName("iphone12");
 		
 		Order2 o=new Order2();
 		o.setPrice(new BigDecimal(20));
@@ -60,7 +60,7 @@ public class ServieTest extends BaseTest {
 	@Test
 	public void testInsertProductAndOrder2() {
 		Product p=new Product();
-		p.setPName("iphon12");
+		p.setPName("iphone12");
 		
 		Order2 o=new Order2();
 		o.setPrice(new BigDecimal(20));
